74f0c15dbfddca3db344d40bb6ada697ec2a4112
ngocthuong26189 <ngocthuong26189@gmail.com>
2014-11-28 13:06:02 +0700

    upgrade

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -39,40 +39,12 @@ def angular(s):
 
 # import webapp modules
 from webapp.views import home
-from webapp.views.api import upload
-from webapp.views.api import comment
-from webapp.views.api import user as api_user
 from webapp.views import image
 from webapp.views.admin import dashboard
-from webapp.views.admin import user
-from webapp.views.admin import category
-from webapp.views.admin import product
-from webapp.views.admin import brand
-from webapp.views.admin import order
-from webapp.views.admin import sticky
-from webapp.views.admin import news_category
-from webapp.views.admin import question_category
-from webapp.views.admin import news
-from webapp.views.admin import question
-from webapp.views.sites import product as sites_product
-from webapp.views.sites import cart
+
 
 # register blueprint to webapp
 app.register_blueprint(home.module)
-app.register_blueprint(upload.module, url_prefix="/api/v1/upload")
-app.register_blueprint(api_user.module, url_prefix="/api/v1/user")
-app.register_blueprint(comment.module, url_prefix="/api/v1/comment")
 app.register_blueprint(image.module, url_prefix="/image")
 app.register_blueprint(dashboard.module, url_prefix="/admin")
-app.register_blueprint(user.module, url_prefix="/admin/user")
-app.register_blueprint(category.module, url_prefix="/admin/category")
-app.register_blueprint(product.module, url_prefix="/admin/product")
-app.register_blueprint(brand.module, url_prefix="/admin/brand")
-app.register_blueprint(order.module, url_prefix="/admin/order")
-app.register_blueprint(sticky.module, url_prefix="/admin/sticky")
-app.register_blueprint(news_category.module, url_prefix="/admin/news_category")
-app.register_blueprint(question_category.module, url_prefix="/admin/question_category")
-app.register_blueprint(news.module, url_prefix="/admin/news")
-app.register_blueprint(question.module, url_prefix="/admin/question")
-app.register_blueprint(sites_product.module, url_prefix="/product")
-app.register_blueprint(cart.module, url_prefix="/cart")
+
